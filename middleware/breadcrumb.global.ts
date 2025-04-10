8ec3d43bf8142ab8ca9e05fafafbb3dc4f5d31ad
ajay-mathur-compro <ajaymathurcompro@gmail.com>
2025-04-10 11:52:55 +0530

    naming

--- middleware/breadcrumb.global.ts
+++ middleware/breadcrumb.global.ts
@@ -2,10 +2,10 @@
 
 export default defineNuxtRouteMiddleware((to, from) => {
   // Access the breadcrumb store
-  const breadcrumbStore = BreadcrumbStore();
+  const store = BreadcrumbStore();
 
   if (to.path === from.path) {
-    breadcrumbStore.resetBreadcrumbs();
+    store.resetBreadcrumbs();
 
     // Split the path into segments and build the breadcrumb trail
     const segments = to.path.split("/").filter(Boolean);
@@ -16,7 +16,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
       const matchedRoute = useRouter().resolve(accumulatedPath);
       const breadcrumbText = matchedRoute?.meta?.title || segment;
       // Add the breadcrumb to the breadcrumb store.
-      breadcrumbStore.addBreadcrumb({
+      store.addBreadcrumb({
         text: String(breadcrumbText),
         url: accumulatedPath,
       });
@@ -28,15 +28,15 @@ export default defineNuxtRouteMiddleware((to, from) => {
     // If the user is navigating to a path other than the root ("/"),
     // add a new breadcrumb to the breadcrumb store.
     if (to.path !== "/") {
-      breadcrumbStore.addBreadcrumb({
+      store.addBreadcrumb({
         text: String(breadcrumbText), // Convert the breadcrumb text to a string.
         url: to.path, // Use the current route path as the breadcrumb URL.
       });
     }
     // If the user is navigating to the root path ("/"),
     // reset the breadcrumbs in the breadcrumb store.
-    if(to.path === "/"){
-      breadcrumbStore.resetBreadcrumbs();
+    if (to.path === "/") {
+      store.resetBreadcrumbs();
     }
   }
 });
